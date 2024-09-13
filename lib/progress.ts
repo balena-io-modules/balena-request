a04c281a175adc71c249697fbc9c190acb2289c2
Thodoris Greasidis <thgreasi@gmail.com>
2024-09-13 13:55:34 +0300

    Drop fetch-ponyfill in favor of native fetch
    Change-type: minor

--- lib/progress.ts
+++ lib/progress.ts
@@ -94,17 +94,10 @@ export interface BalenaRequestPassThroughStream extends Stream.PassThrough {
  * 		stream.on 'progress', (state) ->
  * 			console.log(state)
  */
-export function estimate(
-	requestAsync?: ReturnType<typeof getRequestAsync>,
-	isBrowser?: boolean,
-) {
+export function estimate(requestAsync: ReturnType<typeof getRequestAsync>) {
 	return async function (
 		options: BalenaRequestOptions,
 	): Promise<BalenaRequestPassThroughStream> {
-		if (requestAsync == null) {
-			requestAsync = utils.getRequestAsync();
-		}
-
 		options.gzip = false;
 		options.headers!['Accept-Encoding'] = 'gzip, deflate';
 
@@ -155,51 +148,7 @@ export function estimate(
 			output.emit('progress', state),
 		);
 
-		if (!isBrowser && utils.isResponseCompressed(response)) {
-			// eslint-disable-next-line @typescript-eslint/no-var-requires
-			const zlib = require('zlib') as typeof import('zlib');
-
-			// Be more lenient with decoding compressed responses, since (very rarely)
-			// servers send slightly invalid gzip responses that are still accepted
-			// by common browsers.
-			// Always using Z_SYNC_FLUSH is what cURL does.
-			let zlibOptions = {
-				flush: zlib.constants.Z_SYNC_FLUSH,
-				finishFlush: zlib.constants.Z_SYNC_FLUSH,
-			};
-
-			// Allow overriding this behaviour by setting the ZLIB_FLUSH env var
-			// to one of the allowed zlib flush values (process.env.ZLIB_FLUSH="Z_NO_FLUSH").
-			// https://github.com/nodejs/node/blob/master/doc/api/zlib.md#zlib-constants
-			if (process.env.ZLIB_FLUSH && process.env.ZLIB_FLUSH in zlib.constants) {
-				zlibOptions = {
-					flush:
-						zlib.constants[
-							process.env.ZLIB_FLUSH as keyof typeof zlib.constants
-						],
-					finishFlush:
-						zlib.constants[
-							process.env.ZLIB_FLUSH as keyof typeof zlib.constants
-						],
-				};
-			}
-
-			const gunzip = zlib.createGunzip(zlibOptions);
-			gunzip.on('error', (e) => output.emit('error', e));
-
-			// Uncompress after or before piping through progress
-			// depending on the response length available to us
-			if (
-				responseLength.compressed != null &&
-				responseLength.uncompressed == null
-			) {
-				responseStream.pipe(progressStream).pipe(gunzip).pipe(output);
-			} else {
-				responseStream.pipe(gunzip).pipe(progressStream).pipe(output);
-			}
-		} else {
-			responseStream.pipe(progressStream).pipe(output);
-		}
+		responseStream.pipe(progressStream).pipe(output);
 
 		// Stream any request errors on downstream
 		responseStream.on('error', (e: Error) => output.emit('error', e));
