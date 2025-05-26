0456af703d04895e2a543820b1aa7fff9a6800d1
Thodoris Greasidis <thgreasi@gmail.com>
2025-05-26 19:51:58 +0300

    Convert the stream tests to async await
    Change-type: patch

--- tests/stream.spec.ts
+++ tests/stream.spec.ts
@@ -7,17 +7,17 @@ import * as utils from '../build/utils';
 
 const mockServer = mockhttp.getLocal();
 
-const { auth, request, delay } = setup();
+const { auth, request, delay, IS_BROWSER } = setup();
 const gzip = (contents: string) =>
-	new Promise<Buffer>((resolve, reject) =>
+	new Promise<Buffer>((resolve, reject) => {
 		zlib.gzip(contents, (err, res) => {
 			if (err) {
 				reject(err);
 			} else {
 				resolve(res);
 			}
-		}),
-	);
+		});
+	});
 
 const writeMethods = [
 	['DELETE', 'Delete'],
@@ -37,24 +37,25 @@ describe('Request (stream):', function () {
 			mockServer.forGet('/foo').thenReply(400, 'Something happened'),
 		);
 
-		it('should reject with the error message', function () {
+		it('should reject with the error message', async function () {
 			const promise = request.stream({
 				method: 'GET',
 				baseUrl: mockServer.url,
 				url: '/foo',
 			});
 
-			return expect(promise).to.be.rejectedWith('Something happened');
+			await expect(promise).to.be.rejectedWith('Something happened');
 		});
 
-		it('should have the status code in the error object', () =>
-			expect(
+		it('should have the status code in the error object', async () => {
+			await expect(
 				request.stream({
 					method: 'GET',
 					baseUrl: mockServer.url,
 					url: '/foo',
 				}),
-			).to.be.rejected.then((error) => expect(error.statusCode).to.equal(400)));
+			).to.be.rejected.then((error) => expect(error.statusCode).to.equal(400));
+		});
 	});
 
 	describe('given a simple endpoint that responds with a string', function () {
@@ -62,157 +63,142 @@ describe('Request (stream):', function () {
 			mockServer.forGet('/foo').thenReply(200, 'Lorem ipsum dolor sit amet'),
 		);
 
-		it('should be able to pipe the response', () =>
-			request
-				.stream({
-					method: 'GET',
-					baseUrl: mockServer.url,
-					url: '/foo',
-				})
-				.then(utils.getStreamContents)
-				.then((data) => expect(data).to.equal('Lorem ipsum dolor sit amet')));
+		it('should be able to pipe the response', async () => {
+			const stream = await request.stream({
+				method: 'GET',
+				baseUrl: mockServer.url,
+				url: '/foo',
+			});
+			const data = await utils.getStreamContents(stream);
+			expect(data).to.equal('Lorem ipsum dolor sit amet');
+		});
 
-		it('should be able to pipe the response after a delay', () =>
-			request
-				.stream({
-					method: 'GET',
-					baseUrl: mockServer.url,
-					url: '/foo',
-				})
-				.then((stream) => delay(200).then(() => stream))
-				.then(function (stream) {
-					const pass = new PassThrough();
-					stream.pipe(pass);
-
-					return utils
-						.getStreamContents(pass)
-						.then((data) =>
-							expect(data).to.equal('Lorem ipsum dolor sit amet'),
-						);
-				}));
+		it('should be able to pipe the response after a delay', async () => {
+			const stream = await request.stream({
+				method: 'GET',
+				baseUrl: mockServer.url,
+				url: '/foo',
+			});
+			await delay(200);
+			const pass = new PassThrough();
+			stream.pipe(pass);
+			const data = await utils.getStreamContents(pass);
+			expect(data).to.equal('Lorem ipsum dolor sit amet');
+		});
 	});
 
 	describe('given multiple endpoints', function () {
-		beforeEach(() =>
-			methods.forEach(([upperMethod, camelMethod]) =>
-				mockServer[`for${camelMethod}`]('/foo').thenReply(200, upperMethod),
-			),
-		);
+		beforeEach(async () => {
+			for (const [upperMethod, camelMethod] of methods) {
+				await mockServer[`for${camelMethod}`]('/foo').thenReply(
+					200,
+					upperMethod,
+				);
+			}
+		});
 
-		describe('given no method option', () =>
-			it('should default to GET', () =>
-				request
-					.stream({
-						baseUrl: mockServer.url,
-						url: '/foo',
-					})
-					.then(utils.getStreamContents)
-					.then((data) => expect(data).to.equal('GET'))));
+		describe('given no method option', () => {
+			it('should default to GET', async () => {
+				const stream = await request.stream({
+					baseUrl: mockServer.url,
+					url: '/foo',
+				});
+				const data = await utils.getStreamContents(stream);
+				expect(data).to.equal('GET');
+			});
+		});
 	});
 
 	describe('given a gzip endpoint with an x-transfer-length header', function () {
-		beforeEach(function () {
+		beforeEach(async function () {
 			const message = 'Lorem ipsum dolor sit amet';
-			return gzip(message).then((compressedMessage) =>
-				mockServer.forGet('/foo').thenReply(200, compressedMessage, {
-					'Content-Type': 'text/plain',
-					'X-Transfer-Length': '' + compressedMessage.length,
-					'Content-Encoding': 'gzip',
-				}),
-			);
+			const compressedMessage = await gzip(message);
+			await mockServer.forGet('/foo').thenReply(200, compressedMessage, {
+				'Content-Type': 'text/plain',
+				'X-Transfer-Length': `${compressedMessage.length}`,
+				'Content-Encoding': 'gzip',
+			});
 		});
 
-		it('should correctly uncompress the body', () =>
-			request
-				.stream({
-					baseUrl: mockServer.url,
-					url: '/foo',
-				})
-				.then((stream) => utils.getStreamContents(stream))
-				.then(function (data) {
-					expect(data).to.equal('Lorem ipsum dolor sit amet');
-					return expect(data.length).to.equal(26);
-				}));
+		it('should correctly uncompress the body', async () => {
+			const stream = await request.stream({
+				baseUrl: mockServer.url,
+				url: '/foo',
+			});
+			const data = await utils.getStreamContents(stream);
+			expect(data).to.equal('Lorem ipsum dolor sit amet');
+			expect(data.length).to.equal(26);
+		});
 
-		it('should set no .length property', () =>
-			request
-				.stream({
-					baseUrl: mockServer.url,
-					url: '/foo',
-				})
-				.then((stream) => {
-					// @ts-expect-error We're intentionally testing invalid property
-					expect(stream.length).to.be.undefined;
-				}));
+		it('should set no .length property', async () => {
+			const stream = await request.stream({
+				baseUrl: mockServer.url,
+				url: '/foo',
+			});
+			// @ts-expect-error We're intentionally testing invalid property
+			expect(stream.length).to.be.undefined;
+		});
 	});
 
 	describe('given an gzip endpoint with a content-length header', function () {
-		beforeEach(function () {
+		beforeEach(async function () {
 			const message = 'Lorem ipsum dolor sit amet';
-			return gzip(message).then((compressedMessage) =>
-				mockServer.forGet('/foo').thenReply(200, compressedMessage, {
-					'Content-Type': 'text/plain',
-					'Content-Length': '' + compressedMessage.length,
-					'Content-Encoding': 'gzip',
-				}),
-			);
+			const compressedMessage = await gzip(message);
+			await mockServer.forGet('/foo').thenReply(200, compressedMessage, {
+				'Content-Type': 'text/plain',
+				'Content-Length': `${compressedMessage.length}`,
+				'Content-Encoding': 'gzip',
+			});
 		});
 
-		it('should correctly uncompress the body', () =>
-			request
-				.stream({
-					baseUrl: mockServer.url,
-					url: '/foo',
-				})
-				.then((stream) => utils.getStreamContents(stream))
-				.then(function (data) {
-					expect(data).to.equal('Lorem ipsum dolor sit amet');
-					return expect(data.length).to.equal(26);
-				}));
+		it('should correctly uncompress the body', async () => {
+			const stream = await request.stream({
+				baseUrl: mockServer.url,
+				url: '/foo',
+			});
+			const data = await utils.getStreamContents(stream);
+			expect(data).to.equal('Lorem ipsum dolor sit amet');
+			expect(data.length).to.equal(26);
+		});
 	});
 
 	describe('given an gzip endpoint with a content-length and x-transfer-length headers', function () {
-		beforeEach(function () {
+		beforeEach(async function () {
 			const message = 'Lorem ipsum dolor sit amet';
-			return gzip(message).then((compressedMessage) =>
-				mockServer.forGet('/foo').thenReply(200, compressedMessage, {
-					'Content-Type': 'text/plain',
-					'X-Transfer-Length': '' + compressedMessage.length,
-					'Content-Length': '' + compressedMessage.length,
-					'Content-Encoding': 'gzip',
-				}),
-			);
+			const compressedMessage = await gzip(message);
+			await mockServer.forGet('/foo').thenReply(200, compressedMessage, {
+				'Content-Type': 'text/plain',
+				'X-Transfer-Length': `${compressedMessage.length}`,
+				'Content-Length': `${compressedMessage.length}`,
+				'Content-Encoding': 'gzip',
+			});
 		});
 
-		it('should correctly uncompress the body', () =>
-			request
-				.stream({
-					baseUrl: mockServer.url,
-					url: '/foo',
-				})
-				.then((stream) => utils.getStreamContents(stream))
-				.then(function (data) {
-					expect(data).to.equal('Lorem ipsum dolor sit amet');
-					return expect(data.length).to.equal(26);
-				}));
+		it('should correctly uncompress the body', async () => {
+			const stream = await request.stream({
+				baseUrl: mockServer.url,
+				url: '/foo',
+			});
+			const data = await utils.getStreamContents(stream);
+			expect(data).to.equal('Lorem ipsum dolor sit amet');
+			expect(data.length).to.equal(26);
+		});
 	});
 
 	describe('given an endpoint with a content-type header', function () {
-		beforeEach(function () {
+		beforeEach(async function () {
 			const message = 'Lorem ipsum dolor sit amet';
-			return mockServer.forGet('/foo').thenReply(200, message, {
+			await mockServer.forGet('/foo').thenReply(200, message, {
 				'Content-Type': 'application/octet-stream',
 			});
 		});
 
-		it('should become a stream with a mime property', () =>
-			request
-				.stream({
-					baseUrl: mockServer.url,
-					url: '/foo',
-				})
-				.then((stream) =>
-					expect(stream.mime).to.equal('application/octet-stream'),
-				));
+		it('should become a stream with a mime property', async () => {
+			const stream = await request.stream({
+				baseUrl: mockServer.url,
+				url: '/foo',
+			});
+			expect(stream.mime).to.equal('application/octet-stream');
+		});
 	});
 });
